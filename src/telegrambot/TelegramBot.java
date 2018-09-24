1738968d276847f9d922719e5ad55e2e263d3dc9
Roberto B. Stanziale <stanziale.r@outlook.com>
2018-09-24 22:52:05 +0200

    my first commit

---
+++ src/telegrambot/TelegramBot.java
@@ -0,0 +1,23 @@
+package telegrambot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class TelegramBot {
+
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init(); //Inizializza le api per poter sviluppare
+
+        TelegramBotsApi botsApi = new TelegramBotsApi(); //Crea il nostro bot
+
+        try {
+            botsApi.registerBot(new MyBots()); //Registra il bot con gli attributi che abbiamo dato
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
