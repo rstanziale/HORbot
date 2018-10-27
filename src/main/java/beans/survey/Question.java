fce1d258367a6c627a3d49d3974b70c3e6ed0387
Roberto B. Stanziale <stanziale.r@outlook.com>
2018-10-27 18:57:21 +0200

    Set JavaDoc of the Question and LoggerInterface classes

--- src/main/java/beans/survey/Question.java
+++ src/main/java/beans/survey/Question.java
@@ -10,26 +10,43 @@ public class Question {
     private String question;
     private int answer;
 
+    /**
+     * Constructor of a question and answer
+     * @param question representing the question of the survey
+     * @param answer representing the answer of the survey
+     */
     public Question(String question, int answer) {
         this.question = question;
         this.answer = answer;
     }
 
+    /**
+     * Get the question
+     * @return the question
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * Get the answer of the question
+     * @return the answer of the question
+     */
     public int getAnswer() {
         return answer;
     }
 
+    /**
+     * Set the answer of the question
+     * @param answer of the question
+     */
     public void setAnswer(int answer) {
         this.answer = answer;
     }
 
     @Override
     public String toString() {
-        return "'Q: " + question + '\'' +
+        return "Q: '" + question + '\'' +
                 "\nA: " + answer + '\n';
     }
 }
--- src/main/java/settings/LoggerInterface.java
+++ src/main/java/settings/LoggerInterface.java
@@ -2,6 +2,12 @@ package settings;
 
 import org.apache.log4j.Logger;
 
+/**
+ * Define Logger interface for console messages
+ *
+ * @author Roberto B. Stanziale
+ * @version 1.0
+ */
 public interface LoggerInterface {
 
     // LOGGER
